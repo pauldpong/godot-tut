b82d7d50393845eb767c86b2f904402be68fb66d
Paul D'Pong <pauldamrong@gmail.com>
2020-05-26 22:22:06 -0400

    added acceleration based movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,9 +1,16 @@
 extends KinematicBody2D
 
+const ACCELERATION = 10
+const MAX_SPEED = 100
+
+var velocity = Vector2.ZERO
+
 func _physics_process(delta):
-	var velocity = Vector2.ZERO
+	var input_vector = Vector2.ZERO
 	
-	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	velocity = velocity.move_toward(input_vector.normalized() * MAX_SPEED * delta, ACCELERATION * delta)
 	
 	move_and_collide(velocity)
