c9727e3fd080a44bd2f915d3e49db707c7b22c82
Paul D'Pong <pauldamrong@gmail.com>
2020-05-27 23:00:35 -0400

    Added collision to player model

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
-const ACCELERATION = 10
-const MAX_SPEED = 100
+const ACCELERATION = 500
+const MAX_SPEED = 80
 
 var velocity = Vector2.ZERO
 
@@ -11,6 +11,8 @@ func _physics_process(delta):
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
-	velocity = velocity.move_toward(input_vector.normalized() * MAX_SPEED * delta, ACCELERATION * delta)
+	# speed = pixel/second, and delta = second/frame, we do this 
+	# so we get the relative pixel/frame depending on FPS
+	velocity = velocity.move_toward(input_vector.normalized() * MAX_SPEED, ACCELERATION * delta)
 	
-	move_and_collide(velocity)
+	velocity = move_and_slide(velocity)
