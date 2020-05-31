f654d3841cb48eced11a78ccd1868e42074f99c1
Paul D'Pong <pauldamrong@gmail.com>
2020-05-31 14:28:33 -0400

    added grass & grass effects

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,12 +3,35 @@ extends KinematicBody2D
 const ACCELERATION = 500
 const MAX_SPEED = 80
 
+enum {
+	MOVE,
+	ATTACK,
+	ROLL
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
 
+onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
-func _physics_process(delta):
+func _ready():
+	animationTree.active = true
+
+func _process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+			
+			if Input.is_action_just_pressed("ui_attack"):
+				state = ATTACK
+		ATTACK:
+			attack_state(delta)
+		ROLL:
+			pass
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -18,6 +41,7 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 	
 	animationState.travel("Run" if input_vector != Vector2.ZERO else "Idle")
 	
@@ -26,3 +50,10 @@ func _physics_process(delta):
 	velocity = velocity.move_toward(input_vector.normalized() * MAX_SPEED, ACCELERATION * delta)
 	
 	velocity = move_and_slide(velocity)
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+	
+func on_attack_animation_end():
+	state = MOVE
